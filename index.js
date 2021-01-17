9cacedcaed694d7b413f30327d6e321f69f7a41a
Erik Zhao <erikzhao98@gmail.com>
2021-01-17 19:45:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+<<<<<<< HEAD
 // Your code here
+=======
+>>>>>>> da1857c98cc5490dee9a15b99614e43141e3abd0
 function saturdayFun(activity = 'roller-skate') {
     return `This Saturday, I want to ${activity}!`
 }
 
+<<<<<<< HEAD
 
 let mondayWork = function (activity = "go to the office") {
     return `This Monday, I will ${activity}.`
@@ -10,6 +14,14 @@ let mondayWork = function (activity = "go to the office") {
 
 function wrapAdjective(flair = "*") {
     return function (adj = "special") {
+=======
+let mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair="*") {
+    return function(adj="special") {
+>>>>>>> da1857c98cc5490dee9a15b99614e43141e3abd0
         return `You are ${flair}${adj}${flair}!`
     }
 }
@@ -25,11 +37,22 @@ const Calculator = {
         return 1 * 3
     },
     divide: function() {
+<<<<<<< HEAD
         return 10 / 5 
+=======
+      return 10 / 5
+    },
+    actionApplyer: function(int, arr){
+        
+>>>>>>> da1857c98cc5490dee9a15b99614e43141e3abd0
     }
 }
 
 const actionApplyer = function(int, arr) {
     if (arr.length === 0) {return int}
+<<<<<<< HEAD
     else {return 4}
+=======
+    else{return 4}
+>>>>>>> da1857c98cc5490dee9a15b99614e43141e3abd0
 }
